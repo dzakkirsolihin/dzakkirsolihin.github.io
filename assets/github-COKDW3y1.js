0e45c722272fe848fd31cafb5aa48597e207045b
D. RikaZ <119458243+dzakkirsolihin@users.noreply.github.com>
2025-01-09 20:00:29 +0700

    Updates

--- assets/github-COKDW3y1.js
+++ assets/github-COKDW3y1.js
@@ -1,4 +1,4 @@
-import{c}from"./index--sR3HvKp.js";/**
+import{c}from"./index-Dl4Z8hkd.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/linkedin-8TNXYswk.js
+++ assets/linkedin-8TNXYswk.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index--sR3HvKp.js";/**
+import{c as e}from"./index-Dl4Z8hkd.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
