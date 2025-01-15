3fcb8e9c3c22090fd45effd180ebb53b5d89cdf7
D. RikaZ <119458243+dzakkirsolihin@users.noreply.github.com>
2025-01-15 18:21:04 +0700

    Updates

--- assets/AboutSection-BpiHcYMy.js
+++ assets/AboutSection-BpiHcYMy.js
@@ -1,4 +1,4 @@
-import{c as t,j as e,C as o}from"./index--sR3HvKp.js";/**
+import{c as t,j as e,C as o}from"./index-BkGqFNTp.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/github-CS6Wyr54.js
+++ assets/github-CS6Wyr54.js
@@ -1,4 +1,4 @@
-import{c}from"./index--sR3HvKp.js";/**
+import{c}from"./index-BkGqFNTp.js";/**
  * @license lucide-react v0.469.0 - ISC
  *
  * This source code is licensed under the ISC license.
